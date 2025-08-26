1f4878d51fe220b0863719839fe53fc990f88685
Gonzalo <quaringonzalo@gmail.com>
2025-08-25 21:42:36 -0300

    fix: ensure proper layout height in DashboardLayout component

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -4,8 +4,8 @@ export default function DashboardLayout({ children }) {
   return (
     <div className="min-h-screen bg-gray-50 flex">
       <DashboardSidebar />
-      <div className="flex-1 flex flex-col">
-        {/* <JobSeekerHeader onLogout={handleLogout} /> */}
+      <div className="min-h-screen max-h-screen flex-1 flex flex-col">
+        {/* <JobSeekrHeader onLogout={handleLogout} /> */}
         <main className="flex-1 overflow-auto">{children}</main>
       </div>
     </div>

38a6080f4e4a80ad43b49b64c70db32b0c7ffa30
suley22 <quaringaston@gmail.com>
2025-08-27 20:27:56 -0300

    fix: update redirect path in login function to point to /dashboard/home

--- src/app/login/actions.jsx
+++ src/app/login/actions.jsx
@@ -22,7 +22,7 @@ export async function login(formData) {
   }
 
   revalidatePath("/", "layout");
-  redirect("/dashboard");
+  redirect("/dashboard/home");
 }
 
 export async function signup(formData) {

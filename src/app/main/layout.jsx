95067c0a8d87cec969528d8671045c3886ccc4be
Gonzalo <quaringonzalo@gmail.com>
2025-09-08 19:09:30 -0300

    feat: agregar barra lateral del dashboard al layout para mejorar la navegación y organización del contenido

--- src/app/main/layout.jsx
+++ src/app/main/layout.jsx
@@ -1,16 +1,17 @@
 import { Header } from "@/components/header";
+import DashboardSidebar from "@/components/dashboard-sidebar";
 
 export default function DashboardLayout({ children }) {
   return (
     <>
-    <Header/>
-    <div className="min-h-screen bg-gray-50 flex">
-      {/* <DashboardSidebar/> */}
-      <div className="min-h-screen max-h-screen flex-1 flex flex-col">
-        {/* <JobSeekrHeader onLogout={handleLogout} /> */}
-        <main className="flex-1 overflow-auto">{children}</main>
+      <Header />
+      <div className="min-h-screen bg-gray-50 flex">
+        <DashboardSidebar />
+        <div className="min-h-screen max-h-screen flex-1 flex flex-col">
+          {/* <JobSeekrHeader onLogout={handleLogout} /> */}
+          <main className="flex-1 overflow-auto">{children}</main>
+        </div>
       </div>
-    </div>
     </>
   );
 }

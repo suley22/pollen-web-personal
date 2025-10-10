c40c95ed1dbc343cd1559414c85a458a5be6708a
Gonzalo <quaringonzalo@gmail.com>
2025-10-10 15:00:31 -0300

    Refactor Providers component to simplify user data extraction and utilize default values

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -4,23 +4,29 @@ import { createContext, useContext } from "react";
 
 const UserContext = createContext();
 
+const defaultValues = {
+  name: "Guest User",
+  email: "",
+  avatar: "https://www.gravatar.com/avatar/?d=mp",
+  role: "job_seeker",
+};
+
 export function useUser() {
   return useContext(UserContext);
 }
 
 export function Providers({ children, user }) {
   const session = user?.user || null;
+  const metadata = session?.user_metadata || {};
+  const firstName = metadata.first_name || "";
+  const lastName = metadata.last_name || "";
+  const fullName = firstName + " " + lastName;
 
   const userData = {
-    name:
-      session?.user_metadata?.first_name +
-        " " +
-        session?.user_metadata?.last_name || "Guest User",
-    email: session?.email || "",
-    avatar:
-      session?.user_metadata?.avatar_url ||
-      "https://www.gravatar.com/avatar/?d=mp",
-    role: session?.user_metadata?.role || "job_seeker",
+    name: fullName,
+    email: session?.email || defaultValues.email,
+    avatar: session?.user_metadata?.avatar_url || defaultValues.avatar,
+    role: session?.user_metadata?.role || defaultValues.role,
   };
 
   return (

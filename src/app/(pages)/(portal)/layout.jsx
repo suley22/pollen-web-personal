db4e12da9cf08d0ec0ab18d9530190bfa333ccb4
Gonzalo <quaringonzalo@gmail.com>
2025-10-26 14:24:43 -0300

    feat: remove Header component from RootLayout for cleaner layout structure

--- src/app/(pages)/(portal)/layout.jsx
+++ src/app/(pages)/(portal)/layout.jsx
@@ -2,7 +2,6 @@
 
 import { SidebarProvider, SidebarInset } from "@/components/sidebar/sidebar";
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
-import { Header } from "@/components/ui/header";
 
 export default function RootLayout({ children }) {
   return (
@@ -10,7 +9,6 @@ export default function RootLayout({ children }) {
       <SidebarProvider className="testing-sidebar-layout">
         <AppSidebar />
         <SidebarInset className="bg-gray-50">
-          <Header />
           <div className="flex flex-col items-center justify-items-center px-8">
             {children}
           </div>

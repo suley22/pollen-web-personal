2f61c4072096bf8711b8458a46889440d8692cb9
Nair <aguirrenairayle@gmail.com>
2025-10-26 23:32:30 -0300

    feat: update job creation page to remove action prop and enhance job description tab layout

--- src/app/(pages)/(portal)/admin/jobs/view/_components/job-view-description-tab.tsx
+++ src/app/(pages)/(portal)/admin/jobs/view/_components/job-view-description-tab.tsx
@@ -1,3 +1,52 @@
+import { FormCard } from "@/components/design-system";
+import { Briefcase } from "lucide-react";
+
 export function JobDescriptionTab() {
-  return <div>Job Description Content</div>;
+  return (
+    <div className="flex flex-col gap-6">
+      {/* // Job Overview */}
+      <FormCard
+        title="Job Overview"
+        icon={<Briefcase className="h-5 w-5 text-gray-500" />}
+      >
+        <div className="flex flex-col gap-4">
+          <div>Job Title</div>
+          <div>Company</div>
+          <div className="flex flex-row ">
+            <div className="w-full flex flex-col gap-4">
+              <div>Location</div>
+              <div>Salary</div>
+            </div>
+            <div className="w-full flex flex-col gap-4">
+              <div>Time</div>
+              <div>Type</div>
+            </div>
+          </div>
+          <div className="flex flex-col bg-gray-50 rounded-md p-4 gap-4">
+            <div>Employment Detail</div>
+            <div className="flex flex-row">
+              <div className="w-full flex flex-col gap-4">
+                <div>Responsibilities</div>
+                <div>Qualifications</div>
+              </div>
+              <div className="w-full flex flex-col gap-4">
+                <div>Benefits</div>
+                <div>About Company</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </FormCard>
+
+      {/* //About this Role */}
+      <FormCard
+        title="About this Role"
+        icon={<Briefcase className="h-5 w-5 text-gray-500" />}
+      >
+        About this Role
+      </FormCard>
+
+      {/* Key Responsibilities */}
+    </div>
+  );
 }

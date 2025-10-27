50b1c966402945ec5c53d4f02288cefa5c263999
Nair <aguirrenairayle@gmail.com>
2025-10-27 07:59:49 -0300

    feat: enhance JobPersonaTab layout with improved structure and styling for candidate profile and insights

--- src/app/(pages)/(portal)/admin/jobs/view/_components/job-view-persona-tab.tsx
+++ src/app/(pages)/(portal)/admin/jobs/view/_components/job-view-persona-tab.tsx
@@ -7,8 +7,31 @@ export function JobPersonaTab() {
       title="Employer Persona Questionnaire Results"
       icon={<UserCheck className="h-5 w-5 " />}
     >
-      <div className="flex flex-col bg-blue-50 rounded border  border-blue-200">
-        Ideal Candidate Profile
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col bg-blue-50 rounded-lg border border-blue-200 p-4 gap-3">
+          <div className="font-semibold">Ideal Candidate Profile</div>
+          <div className="flex flex-row gap-1 text-sm">
+            <div className="font-semibold ">Primary Behavioral Type:</div>
+            <div>Primary Behavioral Type</div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="text-sm ">Key Traits:</div>
+            <div className="text-sm  font-extralight">Key Traits</div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="text-sm">Work Style:</div>
+            <div className="text-sm">Work Style</div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="text-sm">Ideal Environment:</div>
+            <div className="text-sm">Ideal Environment</div>
+          </div>
+        </div>
+
+        <div className="flex flex-col bg-gray-50 rounded-lg p-4">
+          <div>Behavioral Insights</div>
+          <div>texto</div>
+        </div>
       </div>
     </FormCard>
   );
